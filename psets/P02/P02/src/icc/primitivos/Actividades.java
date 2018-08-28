92d9689952349620691c3dd8a0f7f2836d490e7f
Pablo Trinidad <pabloatp0204@gmail.com>
2018-08-27 21:22:34 -0500

    PSet 02 - Actividad 2.2

--- psets/P02/P02/src/icc/primitivos/Actividades.java
+++ psets/P02/P02/src/icc/primitivos/Actividades.java
@@ -7,14 +7,40 @@ public class Actividades {
         ImpresoraBinario printer = new ImpresoraBinario();
 
         // 2.1
-        System.out.println("Actividad 2.1");
-        int max = Integer.MAX_VALUE;
-        int maxPlusOne = max + 1;
+        System.out.println("====== Actividad 2.1 ======");
         System.out.println("Variable\t\tBase 10\t\tBase 2");
+
+        int max = Integer.MAX_VALUE;
         System.out.print("max\t\t" + max + "\t");
         printer.imprime(max);
+
+        int maxPlusOne = max + 1;
         System.out.print("max + 1\t\t" + maxPlusOne + "\t");
         printer.imprime(maxPlusOne);
+
+        // 2.2
+        System.out.println("\n\n====== Actividad 2.2 ======");
+        System.out.println("Variable\tBase 2");
+
+        double nan = Double.NaN;
+        System.out.print(nan + "\t\t");
+        printer.imprime(nan);
+
+        double negativeInf = Double.NEGATIVE_INFINITY;
+        System.out.print(negativeInf + "\t");
+        printer.imprime(negativeInf);
+
+        double positiveInf = Double.POSITIVE_INFINITY;
+        System.out.print(positiveInf + "\t");
+        printer.imprime(positiveInf);
+
+        double zero = 0.0;
+        System.out.print(zero + "\t\t");
+        printer.imprime(zero);
+
+        double negativeZero = -0.0;
+        System.out.print(negativeZero + "\t\t");
+        printer.imprime(negativeZero);
     }
 
 }
