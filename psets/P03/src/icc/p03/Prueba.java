c6ccdf34505cb69b6e3c9caca1fe43dc9a34e5a2
Pablo Trinidad <pabloatp0204@gmail.com>
2018-08-29 19:54:27 -0500

    Problem set 03 - Ejercicio 02

---
+++ psets/P03/src/icc/p03/Prueba.java
@@ -0,0 +1,19 @@
+package icc.p03;
+
+public class Prueba {
+
+    public static void main(String[] args) {
+        // Ejercicio 1
+        System.out.println("======= Ejercicio 1 =======");
+        System.out.println("10.0 % 3.0 = " + 10.0 % 3.0);
+
+        // Ejericio 2
+        System.out.println("\n======= Ejercicio 2 =======");
+        float x = 1F;
+        float y = 0.00000001F;
+        float z = x - y;
+        System.out.println("z = " + z);
+
+    }
+
+}
